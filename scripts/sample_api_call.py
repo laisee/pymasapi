89976d4fd970b70203cd6a8b9d9c6663afbb1849
User <tinwald@gmail.com>
2019-02-12 13:26:35 +0800

    added .gitignore, reindexed

--- scripts/sample_api_call.py
+++ scripts/sample_api_call.py
@@ -1,4 +1,6 @@
 import requests
 from pprint import pprint
-response = requests.get('https://api.spotify.com/v1/search?type=artist&q=snoop').json()
-pprint(r.json())
+
+url = 'https://eservices.mas.gov.sg/api/action/datastore/search.json?resource_id=9a51d255-fec1-4aca-9818-b7a47490243c'
+response = requests.get(url)
+pprint(response.json())
