b9fb338e40b79b01aa2eb1e54d09a9a13e33efce
laisee <tinwald@gmail.com>
2019-08-16 18:39:59 +0800

    updated test to pytest format

--- test/test_pymaspi_asiadollar.py
+++ test/test_pymaspi_asiadollar.py
@@ -1,14 +1,15 @@
 import pymasapi.client as client
 
+class Test_AsianDollarAssets:
 
-def test_asian_dollar_assets_monthly():
-    c = client.Client()
-    ''' testing monthly asian dollar asset stats '''
-    data = c.asian_dollar_assets("m", 5)
-    assert data is not None, "data should not be None"
+    def test_asian_dollar_assets_monthly(cls):
+        c = client.Client()
+        ''' testing monthly asian dollar asset stats '''
+        data = c.asian_dollar_assets("m", 5)
+        assert data is not None, "data should not be None"
 
-def test_asian_dollar_assetsannual():
-    c = client.Client()
-    ''' testing annual asian dollar asset stats '''
-    data = c.asian_dollar_assets("y", 5)
-    assert data is not None, "data should not be None"
+    def test_asian_dollar_assetsannual(cls):
+        c = client.Client()
+        ''' testing annual asian dollar asset stats '''
+        data = c.asian_dollar_assets("y", 5)
+        assert data is not None, "data should not be None"
